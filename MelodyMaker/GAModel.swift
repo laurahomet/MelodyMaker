fd5bf7ea2ce7ca8bc537a04679e2dd783c6eebb1
Laura Homet Garcia <36137633+laurahomet@users.noreply.github.com>
2020-11-07 20:21:52 -0800

    Code cleanup

--- MelodyMaker/GAModel.swift
+++ MelodyMaker/GAModel.swift
@@ -9,15 +9,22 @@ import Foundation
 
 class GAModel {
     
-    let model = MelodyModel()
+    enum Status {
+        case initialization
+        case evaluation
+        case generation
+    }
+    
+    let melodyModel = MelodyModel()
     let noteModel = NotesModel()
+    var status:Status = .initialization
     
     func initialization(numNotes: Int) -> [[String]]{
         
         var generatedMelodies = [[String]]()
         
-        for _ in 0...4 {
-            generatedMelodies.append(model.getMelody(numNotes: numNotes))
+        for _ in 0...2 {
+            generatedMelodies.append(melodyModel.getMelody(numNotes: numNotes))
         }
         
         return generatedMelodies
@@ -28,7 +35,24 @@ class GAModel {
         
         var newMelodies = [[String]]()
         
-        //Create wheel from the scores
+        let wheel = createWheel(scores: scores)
+        
+        for _ in 0...melodies.count-1 {
+            
+            let parents         = pickParents(melodies: melodies, scores: scores, wheel: wheel)
+            let crossMelody     = crossover(parents: parents, numNotes: numNotes)
+            let mutatedMelody   = mutation(melody: crossMelody, numNotes: numNotes, note: noteModel.getNote())
+            
+            //Add to new population
+            newMelodies.append(mutatedMelody)
+        }
+        
+        return newMelodies
+    }
+    
+    
+    func createWheel(scores: [Float]) -> [Float] {
+        
         let sum = scores.reduce(0.0,+)
         var percent = [Float]()
         var cum_percent:Float = 0
@@ -38,67 +62,60 @@ class GAModel {
             cum_percent += Float(scores[i])/Float(sum)
         }
         
+        return percent
         
-        for _ in 0...melodies.count-1 {
+    }
+    
+    
+    func pickParents(melodies: [[String]], scores: [Float], wheel: [Float]) -> [[String]] {
+        
+        var parentMelodies = [melodies[0],melodies[1]]
+        
+        for j in 0...1 {
+            let random:Float = Float.random(in: 0...1)
             
-            //Pick two parents
-            var parentMelodies = [melodies[0],melodies[1]]
-            
-            for j in 0...1 {
-                let random:Float = Float.random(in: 0...1)
-                
-                var done:Bool = false
-                for i in 0...scores.count-1 {
-                    if !done && percent[i] > random {
-                        parentMelodies[j] = melodies[i]
-                        done = true
-                    }
+            var done:Bool = false
+            for i in 0...scores.count-1 {
+                if !done && wheel[i] > random {
+                    parentMelodies[j] = melodies[i]
+                    done = true
                 }
             }
+        }
         
-            //Crossover
-            var melody = parentMelodies[0] //Initialize it as the first parent
-            let randomPosition = Int.random(in: 0...numNotes-1)
-            
-            for i in 0...numNotes-1 {
-                if(i < randomPosition) {
-                    melody[i] = parentMelodies[0][i]
-                }
-                else {
-                    melody[i] = parentMelodies[1][i]
-                }
+        return parentMelodies
+    }
+    
+    
+    func crossover(parents: [[String]], numNotes: Int) -> [String] {
+        
+        var melody = parents[0] //Initialize it as the first parent
+        let randomPosition = Int.random(in: 0...numNotes-1) //Version 2.0. Improved: Variable position for insertion of 'parent' notes
+        
+        for i in 0...numNotes-1 {
+            if(i < randomPosition) {
+                melody[i] = parents[0][i]
             }
-        
-            //Mutation
-            
-            for _ in 1...numNotes/4 {
-                let randomNote = Int.random(in: 0...numNotes-1)
-                melody[randomNote] = noteModel.getNote()
+            else {
+                melody[i] = parents[1][i]
             }
-            
-            //Add to new population
-            newMelodies.append(melody)
+        }
         
-        } //End for loop
-        
-        return newMelodies
-    }
-    
-    
-    func createWheel() {
-        
-    }
-    
-    func pickParents() {
-        
-    }
-    
-    func crossover() {
+        return melody
         
     }
     
     
-    func mutation() {
+    func mutation(melody: [String], numNotes: Int, note: String) -> [String] {
+        
+        var newMelody = melody
+        
+        for _ in 1...numNotes/4 {
+            let randomNote = Int.random(in: 0...numNotes-1)
+            newMelody[randomNote] = note
+        }
+        
+        return newMelody
         
     }
     
